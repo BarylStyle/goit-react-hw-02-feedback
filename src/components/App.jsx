c0d256ada4a87ae8a84c5b568f4f95065ddd399b
BarylStyle <mateusz.consulting@gmail.com>
2024-06-12 07:08:58 +0200

    upadate

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,56 @@
-function App() {
+import React, { useState } from 'react';
+import { Component } from "react";
+import FeedbackOptions from './FeedbackOptions/FeedbackOptions';
+import Statistics from './Statistics/Statistics';
+import Section from './Notification/Notification';
+import Notification from './Section/Section';
+
+export const App = () => {
+  const [state, setState] = useState({ good: 0, neutral: 0, bad: 0 });
+
+  const handleFeedback = type => {
+    setState(prevState => ({
+      ...prevState,
+      [type]: prevState[type] + 1,
+    }));
+  };
+
+  const countTotalFeedback = () => {
+    return state.good + state.neutral + state.bad;
+  };
+
+  const countPositiveFeedbackPercentage = () => {
+    const total = countTotalFeedback();
+    return total ? Math.round((state.good / total) * 100) : 0;
+  };
+
+  const total = countTotalFeedback();
+  const positivePercentage = countPositiveFeedbackPercentage();
+
   return (
-    <>
-      <h1>React Homework Template (Vite)</h1>
-    </>
+    <div>
+      <Section title="Please leave feedback">
+        <FeedbackOptions
+          options={['good', 'neutral', 'bad']}
+          onLeaveFeedback={handleFeedback}
+        />
+      </Section>
+
+      <Section title="Statistics">
+        {total > 0 ? (
+          <Statistics
+            good={state.good}
+            neutral={state.neutral}
+            bad={state.bad}
+            total={total}
+            positivePercentage={positivePercentage}
+          />
+        ) : (
+          <Notification message="There is no feedback" />
+        )}
+      </Section>
+    </div>
   );
-}
+};
 
 export default App;
